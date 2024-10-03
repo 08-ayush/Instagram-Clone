e78bcb25360e442e5ffc3c88075231c7571a306e
08-ayush <aayushchauhan008@gmail.com>
2024-10-03 17:19:13 +0530

    final_update-1

--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,155 +1,74 @@
-// import { Avatar, Box, Button, Flex, Skeleton, SkeletonCircle } from "@chakra-ui/react";
-// import { Link } from "react-router-dom";
-// import useFollowUser from "../../hooks/useFollowUser";
-// import { timeAgo } from "../../utils/timeAgo";
-
-// const PostHeader = ({ post, creatorProfile }) => {
-// 	const { handleFollowUser, isFollowing, isUpdating } = useFollowUser(post.createdBy);
-
-// 	return (
-// 		<Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
-// 			<Flex alignItems={"center"} gap={2}>
-// 				{creatorProfile ? (
-// 					<Link to={`/${creatorProfile.username}`}>
-// 						<Avatar src={creatorProfile.profilePicURL} alt='user profile pic' size={"sm"} />
-						
-// 					</Link>
-// 				) : (
-// 					<SkeletonCircle size='10' />
-// 				)}
-
-// 				<Flex fontSize={12} fontWeight={"bold"} gap='2'>
-// 					{creatorProfile ? (
-// 						<Link to={`/${creatorProfile.username}`}>{creatorProfile.username}</Link>
-// 					) : (
-// 						<Skeleton w={"100px"} h={"10px"} />
-// 					)}
-
-// 					<Box color={"gray.500"}>• {timeAgo(post.createdAt)}</Box>
-// 				</Flex>
-// 			</Flex>
-// 			<Box cursor={"pointer"}>
-// 				<Button
-// 					size={"xs"}
-// 					bg={"transparent"}
-// 					fontSize={12}
-// 					color={"blue.500"}
-// 					fontWeight={"bold"}
-// 					_hover={{
-// 						color: "white",
-// 					}}
-// 					transition={"0.2s ease-in-out"}
-// 					onClick={handleFollowUser}
-// 					isLoading={isUpdating}
-// 				>
-// 					{isFollowing ? "Unfollow" : "Follow"}
-// 				</Button>
-// 			</Box>
-// 		</Flex>
-// 	);
-// };
-
-// export default PostHeader;
-
-
-import { Avatar, Box, Button, Flex, Skeleton, SkeletonCircle } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Skeleton,
+  SkeletonCircle,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import useFollowUser from "../../hooks/useFollowUser";
 import { timeAgo } from "../../utils/timeAgo";
 
 const PostHeader = ({ post, creatorProfile }) => {
-	const { handleFollowUser, isFollowing, isUpdating } = useFollowUser(post.createdBy);
+  const { handleFollowUser, isFollowing, isUpdating } = useFollowUser(
+    post.createdBy
+  );
 
-	const images = [
-		"/img1.png",
-		"/img2.png",
-		"/img3.png",
-		"/img4.png",
-		"/google.png",
-		"/microsoft.png",
-		"/playstore.png",
-		"/auth.png",
-		"/img1.png",
-		"/img2.png",
-		"/img3.png",
-	];
+  return (
+    <Flex
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      w={"full"}
+      my={2}
+    >
+      <Flex alignItems={"center"} gap={2}>
+        {creatorProfile ? (
+          <Link to={`/${creatorProfile.username}`}>
+            <Avatar
+              src={creatorProfile.profilePicURL}
+              alt="user profile pic"
+              size={"sm"}
+            />
+          </Link>
+        ) : (
+          <SkeletonCircle size="10" />
+        )}
 
-	return (
-		<>
-			{/* Image Gallery */}
-			<Flex 
-				gap={4} 
-				overflowX={"scroll"} 
-				pb={4}
-				css={{
-					// Hide scrollbar in WebKit-based browsers
-					"&::-webkit-scrollbar": { display: "none" },
-					// Hide scrollbar in Firefox
-					scrollbarWidth: "none",
-					// Enable smooth scrolling
-					scrollBehavior: "smooth",
-					gap: "0",
-				}}
-			>
-				{images.map((src, idx) => (
-					<Box key={idx} minW={"85px"} h={"85px"}>
-						<Avatar 
-							src={src} 
-							alt={`Image ${idx + 1}`} 
-							w={"65px"} 
-						h={"65px"}
-							size="md"  // Adjust size if needed, 'sm', 'md', 'lg', etc.
-						/>
-					</Box>
-				))}
-			</Flex>
+        <Flex fontSize={12} fontWeight={"bold"} gap="2">
+          {creatorProfile ? (
+            <Link to={`/${creatorProfile.username}`}>
+              {creatorProfile.username}
+            </Link>
+          ) : (
+            <Skeleton w={"100px"} h={"10px"} />
+          )}
 
-			{/* Post Header */}
-			<Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
-				<Flex alignItems={"center"} gap={2}>
-					{creatorProfile ? (
-						<Link to={`/${creatorProfile.username}`}>
-							<Avatar src={creatorProfile.profilePicURL} alt='user profile pic' size={"sm"} />
-						</Link>
-					) : (
-						<SkeletonCircle size='10' />
-					)}
-
-					<Flex fontSize={12} fontWeight={"bold"} gap='2'>
-						{creatorProfile ? (
-							<Link to={`/${creatorProfile.username}`}>{creatorProfile.username}</Link>
-						) : (
-							<Skeleton w={"100px"} h={"10px"} />
-						)}
-
-						<Box color={"gray.500"}>• {timeAgo(post.createdAt)}</Box>
-					</Flex>
-				</Flex>
-				<Box cursor={"pointer"}>
-					<Button
-						size={"xs"}
-						bg={"transparent"}
-						fontSize={12}
-						color={"blue.500"}
-						fontWeight={"bold"}
-						_hover={{
-							color: "white",
-						}}
-						transition={"0.2s ease-in-out"}
-						onClick={handleFollowUser}
-						isLoading={isUpdating}
-					>
-						{isFollowing ? "Unfollow" : "Follow"}
-					</Button>
-				</Box>
-			</Flex>
-		</>
-	);
+          <Box color={"gray.500"}>• {timeAgo(post.createdAt)}</Box>
+        </Flex>
+      </Flex>
+      <Box cursor={"pointer"}>
+        <Button
+          size={"xs"}
+          bg={"transparent"}
+          fontSize={12}
+          color={"blue.500"}
+          fontWeight={"bold"}
+          _hover={{
+            color: "white",
+          }}
+          transition={"0.2s ease-in-out"}
+          onClick={handleFollowUser}
+          isLoading={isUpdating}
+        >
+          {isFollowing ? "Unfollow" : "Follow"}
+        </Button>
+      </Box>
+    </Flex>
+  );
 };
 
 export default PostHeader;
-
-
 
 //! Dyanmic Story option
 
@@ -168,9 +87,9 @@ export default PostHeader;
 // 		<>
 // 			{/* Image Gallery */}
 // 			{images.length > 0 && (
-// 				<Flex 
-// 					gap={4} 
-// 					overflowX={"scroll"} 
+// 				<Flex
+// 					gap={4}
+// 					overflowX={"scroll"}
 // 					pb={4}
 // 					css={{
 // 						// Hide scrollbar in WebKit-based browsers
@@ -183,9 +102,9 @@ export default PostHeader;
 // 				>
 // 					{images.map((src, idx) => (
 // 						<Box key={idx} minW={"70px"} h={"70px"}>
-// 							<Avatar 
-// 								src={src} 
-// 								alt={`Image ${idx + 1}`} 
+// 							<Avatar
+// 								src={src}
+// 								alt={`Image ${idx + 1}`}
 // 								size="md"  // Adjust size if needed
 // 							/>
 // 						</Box>
